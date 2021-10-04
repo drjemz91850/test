50b4547ba3c7ec1a58677d46d1f685efb8d43b9b
Olujemibola Oluwasegun <olujemibolasegun@gmail.com>
2021-10-04 18:19:24 +0100

    improved the rebuild file to rebuild based on input and a Handler method 'vcard_filer' to write the new contacts.

--- data.py
+++ data.py
@@ -23,17 +23,27 @@ class Handler:
             phone = get_tel(contact, self.contact.index(contact))
             self.tel_nums.update(phone)
 
-    def contact_name(self) :
+    def contact_name(self):
         for contact in self.contact:
-            name = get_names(contact,self.contact.index(contact))       
+            name = get_names(contact, self.contact.index(contact))
             self.contact_names.update(name)
 
-    def rebuild(self):
-        updated_contacts = []
-        for loc, _  in self.tel_nums.items():
-            updated_contacts.append(self.contact[loc])
-        print(len(updated_contacts))
-        return updated_contacts
+    def rebuild(self, data: dict):
+        contacts = []
+        for loc, _ in data:
+            try:
+                contacts.append(self.contact[loc])
+            except IndexError as error:
+                print(error, "Error during rebuild", sep="\t")
+                continue
+        vcard = []
+        for info in contacts:
+            card = ["BEGIN:VCARD\n", "VERSION:2.1\n"]
+            card += [info_.replace("\n", "") + "\n" for info_ in info]
+            card.append("END:VCARD\n")
+            vcard.append(card)
+
+        return vcard
 
     def __repr__(self):
         rep = f'Contact Created from: {self.file_name} \n Contact List Size: {len(self.contact)}\n' \
@@ -41,3 +51,12 @@ class Handler:
               f'Tel_Nums: {len(self.tel_nums)}\n' \
               f'Contact_Names{len(self.contact_names)}'
         return rep
+
+    def vcard_filer(self, filename: str, contact_list: rebuild, mode: str = "w"):
+        """filename: filename or path to new/old file"""
+        with open(filename, mode) as file:
+            for card in contact_list:
+                for card_ in card:
+                    file.write(card_)
+
+
